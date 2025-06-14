4cab1665eaee259d2f2ca3c4d4f31bc277efa374
Eunum56 <msiaffu123@gmail.com>
2025-06-14 19:26:16 +0530

    Add HelperConfig for TopCryptoDetails

---
+++ script/Functions/TCDHelperConfig.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Script} from "forge-std/Script.sol";
+
+import {TopCryptoDetails} from "src/Functions/TopCryptoDetails.sol";
+
+contract TCDHelperConfig is Script {
+    function run() external {
+        
+    }
+}
